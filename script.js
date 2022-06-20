95413dbf3e45d998d66eb96387ad071d6dafb6b0
FELIPE NOTEBOOK <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-06-19 23:47:05 -0300

    CORRIGIENDO EL COMMIT ANTERIOR

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const palos = ["♠︎ ","♣︎","♥︎","♦︎"];
-let barajaOriginal=[];
-let barajaJuego=[];
 
+let barajaOriginal=[];
 
 class cartas{
     constructor(numero, palo){
@@ -14,43 +13,54 @@ class cartas{
 }
 
 
-function generarMazo(){
-    // genera el mazo de cartas
-    for(let j=0; j<4; j++){
-        for(let i=0; i<13; i++) {
-            let carta = new cartas(i,j);
-            barajaOriginal.push(carta);
-            //carta.preguntarPalo(this);
+function generarMazo(baraja, cantidadDeMazos){
+    // genera Z mazos de cartas
+    for(let z=0; z<cantidadDeMazos; z++){
+        for(let j=0; j<4; j++){
+            for(let i=0; i<13; i++) {
+                let carta = new cartas(i,j);
+                barajaOriginal.push(carta);
+                //carta.preguntarPalo(this);
+            }
         }
     }
    
+        
     // mezcla el mazo de cartas
-    
     while(barajaOriginal.length>0){
         let pos = parseInt(Math.random()*(barajaOriginal.length)) ;
-        barajaJuego.push(barajaOriginal[pos]);        
+        baraja.push(barajaOriginal[pos]);        
         barajaOriginal.splice(pos,1);
     }
+    // imprime la baraja generada
 
-    // imprime el mazo de cartas
-    for(let i=0; i<barajaJuego.length; i++) {
-       // barajaJuego[i].preguntarPalo(this);
-    }
-   
+    /*for(let i=0 ; i<baraja.length ; i++){
+        baraja[i].preguntarPalo(this);
+    }*/
+    
 }
-function pedirCartas(cantidad){
+function pedirCartas(cantidad, baraja){
     for(let i=0; i<cantidad; i++){
-        barajaJuego[i].preguntarPalo(this);
+        baraja[i].preguntarPalo(this);
     }
 }
     
-generarMazo();
+
+
+let mazo=[];
+const cantidadDeMazos = 2;
+generarMazo(mazo, cantidadDeMazos);
 
 let cant;
+
 
 do{
     cant=prompt("Cuantas cartas deseas?");
 }while(cant>52 || cant<0);
 
+pedirCartas(cant, mazo);
 
-pedirCartas(cant);
+
+    
+
+    
