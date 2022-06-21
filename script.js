bcda8cfe7b8d0676bb229b00ea27e5fbc364dc97
Felipe7575 <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-06-21 12:42:01 -0300

    REPARTE CARTAS Y SUMA PUNTOS

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function generarMazo(baraja, cantidadDeMazos){
     // genera Z mazos de cartas
     for(let z=0; z<cantidadDeMazos; z++){
         for(let j=0; j<4; j++){
-            for(let i=0; i<13; i++) {
+            for(let i=1; i<14; i++) {
                 let carta = new cartas(i,j);
                 barajaOriginal.push(carta);
                 //carta.preguntarPalo(this);
@@ -39,26 +39,87 @@ function generarMazo(baraja, cantidadDeMazos){
     }*/
     
 }
-function pedirCartas(cantidad, baraja){
+function pedirCartas(cantidad, baraja,cartasJugador){
     for(let i=0; i<cantidad; i++){
-        baraja[i].preguntarPalo(this);
+        cartasJugador.push(baraja[i]);
     }
+    return cartasJugador;
 }
+
+
+
+function sumarPuntos(cartasJugador){
+    let puntos=0;
+    for(let i = 0 ; i<cartasJugador.length; i++){
+        if(cartasJugador[i].numero>=2){
+            puntos+=cartasJugador[i].numero;
+        }
+    }
+    for(let i = 0 ; i<cartasJugador.length; i++){
+        if(cartasJugador[i].numero==1){
+            if(puntos+11 <= 21){
+                puntos+=11;
+            }
+            else{
+                puntos++;
+            }
+        }
+    }
+    return puntos;
+};
     
+function juegaJugador(mazo){
+    let cant;
+    let cartasJugador = [];
+    let puntosActual=0;
+
+    cartasJugador = pedirCartas(2, mazo, cartasJugador);
+    for(let i=0; i<cartasJugador.length; i++){
+        cartasJugador[i].preguntarPalo(this);
+    }
+    for(let i=0; i<2; i++){
+        mazo.shift();
+    }
+    puntosActual = sumarPuntos(cartasJugador);  
+    console.log("--------------------------");
+    if(puntosActual<21){
+        cant=prompt("Plantarse =-1 Carta = 1?");
+        while(cant!=-1 && puntosActual<21){
+            cartasJugador = pedirCartas(cant, mazo, cartasJugador);
+            for(let i=0; i<cant; i++){
+                mazo.shift();
+            }
+            for(let i=0; i<cartasJugador.length; i++){
+                cartasJugador[i].preguntarPalo(this);
+            }
+        
+            puntosActual = sumarPuntos(cartasJugador);  
+            console.log("--------------------------");
+
+            if(puntosActual<21){
+                cant=prompt("Plantarse =-1 Carta = 1?"); 
+            }
+        }
+     }
+
+    return puntosActual;
+}
+
 
 
 let mazo=[];
-const cantidadDeMazos = 2;
+const cantidadDeMazos = 1;
+let puntosJugador=0;
+
 generarMazo(mazo, cantidadDeMazos);
 
-let cant;
+puntosJugador = juegaJugador(mazo);
+console.log(puntosJugador)
 
 
-do{
-    cant=prompt("Cuantas cartas deseas?");
-}while(cant>52 || cant<0);
 
-pedirCartas(cant, mazo);
+
+
 
 
     
