3ec67130eff6225314e0d4e293ca9a82784c9455
Felipe7575 <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-07-14 22:05:26 -0300

    Separando el codigo en DOM y Logica

--- script.js
+++ script.js
@@ -141,168 +141,33 @@ function eligeGanador(puntosJugador,puntosCrupier,cartelGanaPierde){
         if(puntosCrupier<=21){
             if(puntosCrupier>puntosJugador){
                 cartelGanaPierde.className="CartelGanaPierde CartelGana";
+                mueveCartel(cartelGanaPierde)
                 return 1;
             }
             if(puntosCrupier<puntosJugador){
                 cartelGanaPierde.className="CartelGanaPierde CartelPierde";
+                mueveCartel(cartelGanaPierde)
                 return 2;
             }
             if(puntosCrupier===puntosJugador){
                 cartelGanaPierde.className="CartelGanaPierde CartelEmpate";
+                mueveCartel(cartelGanaPierde)
                 return 3;
             }
         }
         else{
             cartelGanaPierde.className="CartelGanaPierde CartelPierde";
+            mueveCartel(cartelGanaPierde)
             return 2;
         }
     }
     else{
         cartelGanaPierde.className="CartelGanaPierde CartelGana";
+        mueveCartel(cartelGanaPierde)
         return 1;
-    }
-        
+    }      
 }
-// La funcion realiza:
-//     Revisa si hay que crear nuevos espacios para cartas (DIV)
-//     Actualiza las cartas en pantalla
-//     Borra los espacios extras al reiniciar una partida (Borra los DIVS creados)
-function actualizaCartas(mazoJugador,cartasCrupier,reiniciar, juegaCrupier){
-    let figuraCrupier =[];
-    let figuraJugador =[];
-    let j=1;
-    figuraCrupier=document.getElementsByClassName("cartaCrup"); 
-    figuraJugador=document.getElementsByClassName("cartaJug"); 
-    // Revisa si la cantidad de cartas en la pantalla ("CANTIDAD DE DIVS") es mayor a la cantidad de 
-    // cartas por mazo de jugador y crupier. Si la CANT es menor se crea nuevos divs
-    if(figuraJugador.length < mazoJugador.length || figuraCrupier.length < cartasCrupier.length){
-        if(figuraJugador.length < mazoJugador.length ){
-            const cartaNueva = document.getElementById("divMesaJugador");
-            let modeloCarta = document.createElement("div");
-            modeloCarta.className = "col  m-3 "; 
-            modeloCarta.innerHTML = ` <div class="carta cartaJug" >
-                                      </div>
-                                     `;
-            cartaNueva.appendChild(modeloCarta);
-        }
-        if(figuraCrupier.length < cartasCrupier.length){
-            const repe = cartasCrupier.length-figuraCrupier.length
-            
-            for(let i=0; i< repe ; i++) {
-                let cartaNueva = document.getElementById("divMesaCrup");
-                let modeloCarta = document.createElement("div");
-                modeloCarta.className = "col  m-3 "; 
-                modeloCarta.innerHTML = ` <div class="carta cartaCrup" >
-                                        </div>
-                                        `;                    
-                cartaNueva.appendChild(modeloCarta);
-            }
-                
-        }
-    }
-    // Pregunta si el usuario apreto el boton Reiniciar 
-    // reiniciar = 0 actualiza las cartas en tablero
-    // reiniciar = 1 borra las cartas del tablero y devuelve la cantidad de figuras de cartas a 4 para el jugador y el crupier
-    // Cada vez que se llama a mueveCarta se le envian los siguientes parametros:
-    // mueveCartas(nodoCarta,X,Y,Z,rotate_X,rotate_Y,time)
-    //             CartaAMover: nodoCarta
-    //             X: vector ofset en X
-    //             Y: vector ofset en Y
-    //             Z: vector ofset en Z
-    //             rotate_X: vector rotate_X
-    //             rotate_Y: vector rotate_Y
-    //             time: tiempo de duracion de la animacion
-    if(reiniciar==0){
-        if(mazoJugador.length == 2 && cartasCrupier.length == 1){
-            figuraJugador[0].className = "carta cartaJug" + " "+"cart"+mazoJugador[0].numero+"-"+mazoJugador[0].palo;
-            mueveCartas(figuraJugador[0],[0, 100],[-1000, 0],[0,0],[0,0],[0,0],500);
-            figuraJugador[1].className = "carta cartaJug" + " "+"cart"+mazoJugador[1].numero+"-"+mazoJugador[1].palo; 
-            mueveCartas(figuraJugador[1],[0, -50],[-1000, 0],[0,0],[0,0],[0,0],500); 
-        }
-        else if(cartasCrupier.length == 1){
-            const num = mazoJugador.length - 1;
-            figuraJugador[num].className = "carta cartaJug" + " "+"cart"+mazoJugador[num].numero+"-"+mazoJugador[num].palo; 
-            mueveCartas(figuraJugador[num],[0, -num*100*1.1],[-1000, 0],[0,0],[0,0],[0,0],500);
-        }
-        const num = cartasCrupier.length;
-        for (let i = 1; i < num; i++) {
-            figuraCrupier[i].className = "carta cartaCrup" + " "+"cart"+cartasCrupier[i].numero+"-"+cartasCrupier[i].palo;      
-            i==1? mueveCartas(figuraCrupier[i],[0, -50],[0, 0],[0,0],[0, 180],[0, 180],300) :
-                  mueveCartas(figuraCrupier[i],[0, -i*100*1.1],[-1000, 0],[0,0],[0,0],[0,0],500+i*100);
-        }
-        if(cartasCrupier.length==1 && juegaCrupier == 1){
-            figuraCrupier[0].className = "carta cartaCrup" + " "+"cart"+cartasCrupier[0].numero+"-"+cartasCrupier[0].palo;
-            mueveCartas(figuraCrupier[0],[0, 100],[-1000, 0],[0,0],[0,0],[0,0],500); 	
-            figuraCrupier[1].className = "carta cartaCrup"+ " "+"backCarta";
-            mueveCartas(figuraCrupier[1],[0, -50],[-1000, 0],[0,0],[0,0],[0,0],500); 		
-        }   
-    }
-    else{
-        const contenedor = document.getElementById("contenedorDeCartas");
-        //Devuelve el html al estado inicial
-        contenedor.innerHTML= ` <div class="row mesa mesaCrup  " id="divMesaCrup">
-                                    <div class="col m-3  ">
-                                        <div class="carta cartaCrup" >
 
-                                        </div>
-                                    </div>
-                                    <div class="col  m-3  ">
-                                        <div class="carta cartaCrup" >
-
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <div class="row mesa mesaJugador " id="divMesaJugador">
-                                        <div class="col  m-3  ">
-                                            <div class="carta cartaJug" >
-
-                                            </div>
-                                        </div>
-                                        <div class="col  m-3  ">
-                                            <div class="carta cartaJug" >
-
-                                            </div>
-                                        </div>
-                                        
-                                </div>`; 
-    }
-}
-// Funcion que mueve las cartas de una posicion a otra utilizando la libreria Anime.js
-function mueveCartas(nodoCarta,X,Y,Z,rotate_X,rotate_Y,time){
-    anime({
-        targets: nodoCarta,
-        translateX: X,
-        translateY: Y,
-        translateZ: Z,
-        rotateX: rotate_X,
-        rotateY: rotate_Y,
-        easing: 'easeInOutQuad',
-        duration: time,
-    }) 
-}
-// La funcion actualiza los carteles de Saldo y Monto apostado
-function actualizarMontos(montoApostado,saldoActual){
-    if(document.getElementsByClassName("cartel").length == 0){
-        const div = document.createElement("div");
-        div.className   = "contenedorSaldos";
-        const cartelSaldo = document.createElement("h3");
-        cartelSaldo.innerHTML = `Saldo actual ${saldoActual}$$`;
-        cartelSaldo.className = "cartel";
-        cartelSaldo.id = "cartelSaldo";
-        const cartelApostando = document.createElement("h3");
-        cartelApostando.innerHTML = `Saldo apostado ${montoApostado}$$`;
-        cartelApostando.className = "cartel";
-        cartelApostando.id = "cartelApostando";
-        div.append(cartelSaldo,cartelApostando);
-        listaApuestas.append(div); 
-    }
-    let cartel = document.getElementById("cartelSaldo");
-    cartel.innerHTML = `Saldo actual ${saldoActual}$$`;
-    cartel = document.getElementById("cartelApostando");
-    cartelApostando.innerHTML = `Saldo apostado ${montoApostado}$$`;
-}
 //Funcion que maneja el localStorage item Saldo
 //Si se envida un valor numerico mayor a cero se lo guarda
 //si se envia -1 retorna el saldo actual
@@ -350,6 +215,7 @@ function administrPago(){
                 showCancelButton: true,
                 cancelButtonText: 'Cancelar',
                 }).then((result) => {
+                    //Si se acepta el pago se acreta el saldo, caso contrario se borra el valor ingresado
                     if(result.isConfirmed){
                         Swal.fire('Pago confirmado', '', 'success');
                         const saldoNuevo = saldoLocalStorage(-1) + parseInt(montoINPUT.value);
@@ -382,22 +248,10 @@ let mazo=[];
 let cartasCrupier=[];
 let cartasJugador=[];
 
-
 //localStorage.setItem("saldo",100);
 // Revisa si tiene saldo anterior, sino lo pone en 0
 saldoLocalStorage(-1);
-// -----------------BUSCA LOS ELEMENTOS DEL DOM QUE SE ALTERAN DURANTE EL JUEGO---------------------------------------------------
 
-const botonPedir=document.getElementById("pedir");
-const botonDoblar=document.getElementById("doblar");
-const botonPlantarse=document.getElementById("plantarse");
-const botonReiniciar=document.getElementById("reiniciar");
-const cartelGanaPierde=document.getElementById("GanaPierde");
-const menuDeApuestas = document.getElementById("listaApuestas");
-const menuDeBotones = document.getElementById("listaDeBotones");
-const cartelCargarDinero = document.getElementById("credit-card-div");
-const mesaDeApuestas = document.getElementById("mesaDeApuestas");
-const mesaDeJuego = document.getElementById("mesaDeJuego");
 // ------------------INICIALIZA VARIABLES----------------------------------------------
 const cantidadDeMazos = 1;
 let puntosJugador=0;
@@ -421,6 +275,16 @@ apuestas.forEach( (apuesta) => {
             saldoLocalStorage(saldoLocalStorage(-1)-apuesta); 
             actualizarMontos(montoApostado,saldoLocalStorage(-1))
         }
+        else{
+            const cartelSaldo = document.getElementById("cartelSaldo");
+            anime({
+                targets: cartelSaldo,
+                translateY: [0, -10],
+                direction: 'reverse',
+                loop: 3,
+                duration: 200,      
+            });
+        }
     }
 }); 
 actualizarMontos(montoApostado,saldoLocalStorage(-1));
@@ -429,8 +293,8 @@ botonApostar.innerHTML = 'Apostar';
 botonApostar.className = "botonApostar btn btn-danger";
 listaApuestas.appendChild(botonApostar);
 botonApostar.onclick = () => { 
-    mesaDeApuestas.className = " MesaDeApuestas container d-none ";
-    mesaDeJuego.className = " container-fluid MesaDeJuego";
+    // Reinicia el DOM (Oculta la mesa de apuesta y muestra la mesa de juego)
+    cambiaMesaJuego_Apuesta(false);
 }
 // GENERNERA LOS EVENT LISTENER DE LOS PAGOS
 administrPago();
@@ -519,12 +383,9 @@ botonReiniciar.onclick = () => {
 
         //Borra las cartas de las partidas anteriores
         actualizaCartas(cartasJugador,cartasCrupier,1,0);
-        cartelGanaPierde.className="CartelGanaPierdeHide ";
 
-        cartelCargarDinero.className = "credit-card-div";
-        mesaDeApuestas.className = " MesaDeApuestas container  ";
-        mesaDeJuego.className = " container-fluid MesaDeJuego d-none";
-       
+        // Reinicia el DOM (Oculta la mesa de juego y muestra la de apuestas)
+        cambiaMesaJuego_Apuesta(true);
     }
 }
 
---
+++ scriptDOM.js
@@ -0,0 +1,185 @@
+
+// -----------------BUSCA LOS ELEMENTOS DEL DOM QUE SE ALTERAN DURANTE EL JUEGO---------------------------------------------------
+const botonPedir=document.getElementById("pedir");
+const botonDoblar=document.getElementById("doblar");
+const botonPlantarse=document.getElementById("plantarse");
+const botonReiniciar=document.getElementById("reiniciar");
+const cartelGanaPierde=document.getElementById("GanaPierde");
+const menuDeApuestas = document.getElementById("listaApuestas");
+const menuDeBotones = document.getElementById("listaDeBotones");
+const cartelCargarDinero = document.getElementById("credit-card-div");
+const mesaDeApuestas = document.getElementById("mesaDeApuestas");
+const mesaDeJuego = document.getElementById("mesaDeJuego");
+const formTarjeta = document.getElementById("collapseExample");
+
+
+// Funcion que cambia entre mesa de apuestas y mesa de juego
+function cambiaMesaJuego_Apuesta (modo){
+    if(modo){
+        cartelGanaPierde.className="CartelGanaPierdeHide ";
+        cartelCargarDinero.className = "credit-card-div";
+        mesaDeApuestas.className = "MesaDeApuestas container align-items-center";
+        mesaDeJuego.className = " container-fluid MesaDeJuego d-none";
+        formTarjeta.className = "collapse";
+    }
+    else{
+        mesaDeApuestas.className = " MesaDeApuestas container d-none ";
+        mesaDeJuego.className = " container-fluid MesaDeJuego";
+    }
+    
+}
+
+// Funcion que mueve el cartel de gana o pierde la casa
+function mueveCartel(cartelGanaPierde){
+    anime(
+        {
+            targets: cartelGanaPierde,
+            translateY: [-1000, 50],
+            
+            duration: 1000
+        }
+    );
+}
+
+// La funcion realiza:
+//     Revisa si hay que crear nuevos espacios para cartas (DIV)
+//     Actualiza las cartas en pantalla
+//     Borra los espacios extras al reiniciar una partida (Borra los DIVS creados)
+function actualizaCartas(mazoJugador,cartasCrupier,reiniciar, juegaCrupier){
+    let figuraCrupier =[];
+    let figuraJugador =[];
+    let j=1;
+    figuraCrupier=document.getElementsByClassName("cartaCrup"); 
+    figuraJugador=document.getElementsByClassName("cartaJug"); 
+    // Revisa si la cantidad de cartas en la pantalla ("CANTIDAD DE DIVS") es mayor a la cantidad de 
+    // cartas por mazo de jugador y crupier. Si la CANT es menor se crea nuevos divs
+    if(figuraJugador.length < mazoJugador.length || figuraCrupier.length < cartasCrupier.length){
+        if(figuraJugador.length < mazoJugador.length ){
+            const cartaNueva = document.getElementById("divMesaJugador");
+            let modeloCarta = document.createElement("div");
+            modeloCarta.className = "col  m-3 "; 
+            modeloCarta.innerHTML = ` <div class="carta cartaJug" >
+                                      </div>
+                                     `;
+            cartaNueva.appendChild(modeloCarta);
+        }
+        if(figuraCrupier.length < cartasCrupier.length){
+            const repe = cartasCrupier.length-figuraCrupier.length
+            
+            for(let i=0; i< repe ; i++) {
+                let cartaNueva = document.getElementById("divMesaCrup");
+                let modeloCarta = document.createElement("div");
+                modeloCarta.className = "col  m-3 "; 
+                modeloCarta.innerHTML = ` <div class="carta cartaCrup" >
+                                        </div>
+                                        `;                    
+                cartaNueva.appendChild(modeloCarta);
+            }
+                
+        }
+    }
+    // Pregunta si el usuario apreto el boton Reiniciar 
+    // reiniciar = 0 actualiza las cartas en tablero
+    // reiniciar = 1 borra las cartas del tablero y devuelve la cantidad de figuras de cartas a 4 para el jugador y el crupier
+    // Cada vez que se llama a mueveCarta se le envian los siguientes parametros:
+    // mueveCartas(nodoCarta,X,Y,Z,rotate_X,rotate_Y,time)
+    //             CartaAMover: nodoCarta
+    //             X: vector ofset en X
+    //             Y: vector ofset en Y
+    //             Z: vector ofset en Z
+    //             rotate_X: vector rotate_X
+    //             rotate_Y: vector rotate_Y
+    //             time: tiempo de duracion de la animacion
+    if(reiniciar==0){
+        if(mazoJugador.length == 2 && cartasCrupier.length == 1){
+            figuraJugador[0].className = "carta cartaJug" + " "+"cart"+mazoJugador[0].numero+"-"+mazoJugador[0].palo;
+            mueveCartas(figuraJugador[0],[0, 100],[-1000, 0],[0,0],[0,0],[0,0],500);
+            figuraJugador[1].className = "carta cartaJug" + " "+"cart"+mazoJugador[1].numero+"-"+mazoJugador[1].palo; 
+            mueveCartas(figuraJugador[1],[0, -50],[-1000, 0],[0,0],[0,0],[0,0],500); 
+        }
+        else if(cartasCrupier.length == 1){
+            const num = mazoJugador.length - 1;
+            figuraJugador[num].className = "carta cartaJug" + " "+"cart"+mazoJugador[num].numero+"-"+mazoJugador[num].palo; 
+            mueveCartas(figuraJugador[num],[0, -num*100*1.1],[-1000, 0],[0,0],[0,0],[0,0],500);
+        }
+        const num = cartasCrupier.length;
+        for (let i = 1; i < num; i++) {
+            figuraCrupier[i].className = "carta cartaCrup" + " "+"cart"+cartasCrupier[i].numero+"-"+cartasCrupier[i].palo;      
+            i==1? mueveCartas(figuraCrupier[i],[0, -50],[0, 0],[0,0],[0, 180],[0, 180],300) :
+                  mueveCartas(figuraCrupier[i],[0, -i*100*1.1],[-1000, 0],[0,0],[0,0],[0,0],500+i*100);
+        }
+        if(cartasCrupier.length==1 && juegaCrupier == 1){
+            figuraCrupier[0].className = "carta cartaCrup" + " "+"cart"+cartasCrupier[0].numero+"-"+cartasCrupier[0].palo;
+            mueveCartas(figuraCrupier[0],[0, 100],[-1000, 0],[0,0],[0,0],[0,0],500); 	
+            figuraCrupier[1].className = "carta cartaCrup"+ " "+"backCarta";
+            mueveCartas(figuraCrupier[1],[0, -50],[-1000, 0],[0,0],[0,0],[0,0],500); 		
+        }   
+    }
+    else{
+        const contenedor = document.getElementById("contenedorDeCartas");
+        //Devuelve el html al estado inicial
+        contenedor.innerHTML= ` <div class="row mesa mesaCrup  " id="divMesaCrup">
+                                    <div class="col m-3  ">
+                                        <div class="carta cartaCrup" >
+
+                                        </div>
+                                    </div>
+                                    <div class="col  m-3  ">
+                                        <div class="carta cartaCrup" >
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="row mesa mesaJugador " id="divMesaJugador">
+                                        <div class="col  m-3  ">
+                                            <div class="carta cartaJug" >
+
+                                            </div>
+                                        </div>
+                                        <div class="col  m-3  ">
+                                            <div class="carta cartaJug" >
+
+                                            </div>
+                                        </div>
+                                        
+                                </div>`; 
+    }
+}
+
+// Funcion que mueve las cartas de una posicion a otra utilizando la libreria Anime.js
+function mueveCartas(nodoCarta,X,Y,Z,rotate_X,rotate_Y,time){
+    anime({
+        targets: nodoCarta,
+        translateX: X,
+        translateY: Y,
+        translateZ: Z,
+        rotateX: rotate_X,
+        rotateY: rotate_Y,
+        easing: 'easeInOutQuad',
+        duration: time,
+    }) 
+}
+
+// La funcion actualiza los carteles de Saldo y Monto apostado
+function actualizarMontos(montoApostado,saldoActual){
+    if(document.getElementsByClassName("cartel").length == 0){
+        const div = document.createElement("div");
+        div.className   = "contenedorSaldos";
+        const cartelSaldo = document.createElement("h3");
+        cartelSaldo.innerHTML = `Saldo actual ${saldoActual}$$`;
+        cartelSaldo.className = "cartel";
+        cartelSaldo.id = "cartelSaldo";
+        const cartelApostando = document.createElement("h3");
+        cartelApostando.innerHTML = `Saldo apostado ${montoApostado}$$`;
+        cartelApostando.className = "cartel";
+        cartelApostando.id = "cartelApostando";
+        div.append(cartelSaldo,cartelApostando);
+        listaApuestas.append(div); 
+    }
+    let cartel = document.getElementById("cartelSaldo");
+    cartel.innerHTML = `Saldo actual ${saldoActual}$$`;
+    cartel = document.getElementById("cartelApostando");
+    cartelApostando.innerHTML = `Saldo apostado ${montoApostado}$$`;
+}
