9634fc7c1e5faada59db889fdb0b6c4b220a2a79
Felipe7575 <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-07-24 15:02:09 -0300

    Agregando asincronismo

--- js/script.js
+++ js/script.js
@@ -168,81 +168,7 @@ function eligeGanador(puntosJugador,puntosCrupier,cartelGanaPierde){
     }      
 }
 
-//Funcion que maneja el localStorage item Saldo
-//Si se envida un valor numerico mayor a cero se lo guarda
-//si se envia -1 retorna el saldo actual
-function saldoLocalStorage(escribe) {
-    let saldoActual;
-    //Se retorna el saldo actual
-    if(escribe == -1){
-        saldoActual = localStorage.getItem("saldo");
-        if(saldoActual === null){
-            saldoActual = 0 ;
-            localStorage.setItem("saldo",0);
-        }
-        else{
-            saldoActual = parseInt(localStorage.getItem("saldo"));
-        }
 
-        return saldoActual;
-    }
-    else{
-        //Se actualiza el saldo en el localStorage
-        localStorage.setItem("saldo",escribe);
-    }  
-}
-//Funcion que administra los ingresos de saldo 
-function administrPago(){
-    const montoINPUT = document.getElementById("montoDepositar");
-    const numeroTarjetaINPUT = document.getElementById("numeroTarjeta");
-    const mesINPUT = document.getElementById("MM");
-    const añoINUPUT = document.getElementById("YY");
-    const ccvINPUT = document.getElementById("CCV");
-    const nombreINPUT = document.getElementById("nombreTarjeta");
-    const guardarINPUT = document.getElementById("guardarTarjeta");
-    const CANCELAR = document.getElementById("botonCancelar");
-    const PAGAR = document.getElementById("botonPagar");
-    const saldoLABEL = document.getElementById("cartelSaldo");
-
-    PAGAR.onclick = () => {
-        if(parseInt(montoINPUT.value)>0){
-            //Cartel Confirmar Pago
-            Swal.fire({
-                title: 'Confirmar Pago',
-                text: '¿Deseas confirmar el pago?',
-                icon: 'warning',
-                confirmButtonText: 'Aceptar',
-                showCancelButton: true,
-                cancelButtonText: 'Cancelar',
-                }).then((result) => {
-                    //Si se acepta el pago se acreta el saldo, caso contrario se borra el valor ingresado
-                    if(result.isConfirmed){
-                        Swal.fire('Pago confirmado', '', 'success');
-                        const saldoNuevo = saldoLocalStorage(-1) + parseInt(montoINPUT.value);
-                        saldoLocalStorage(saldoNuevo);
-                        saldoLABEL.innerHTML = `Saldo actual ${saldoNuevo}$$`;
-                        montoINPUT.value = "";
-                    }
-                    else{
-                        Swal.fire('Pago cancelado', '', 'error');
-                        montoINPUT.value = "";
-                    }
-                });
-        }
-        else{
-            montoINPUT.value = "";  
-            Swal.fire('Pago cancelado', 'El monto ingresado no es valido', 'error');
-        }    
-    }
-    CANCELAR.onclick = () => {
-        montoINPUT.value = "";
-        numeroTarjetaINPUT.value = "";
-        añoINUPUT.value = "";
-        mesINPUT.value = "";
-        ccvINPUT.value = "";
-        nombreINPUT.value = "";
-    }
-}
 
 let mazo=[];
 let cartasCrupier=[];
@@ -387,6 +313,33 @@ botonReiniciar.onclick = () => {
         // Reinicia el DOM (Oculta la mesa de juego y muestra la de apuestas)
         cambiaMesaJuego_Apuesta(true);
     }
+}
+
+//Funcion que maneja el localStorage item Saldo
+//Si se envida un valor numerico mayor a cero se lo guarda
+
+
+
+//si se envia -1 retorna el saldo actual
+function saldoLocalStorage(escribe) {
+    let saldoActual;
+    //Se retorna el saldo actual
+    if(escribe == -1){
+        saldoActual = localStorage.getItem("saldo");
+        if(saldoActual === null){
+            saldoActual = 0 ;
+            localStorage.setItem("saldo",0);
+        }
+        else{
+            saldoActual = parseInt(localStorage.getItem("saldo"));
+        }
+
+        return saldoActual;
+    }
+    else{
+        //Se actualiza el saldo en el localStorage
+        localStorage.setItem("saldo",escribe);
+    }  
 }
 
 
