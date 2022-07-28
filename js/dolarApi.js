a450d1b71c5fdc07189991ce0eb5263607261e8c
Felipe7575 <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-07-28 18:16:30 -0300

    Entrega final

--- js/dolarApi.js
+++ js/dolarApi.js
@@ -1,8 +1,8 @@
 //Script necesario para recibir el precio del DOLAR mediante la API
 
 const etiqueta = document.getElementById("tag_cotiza");
-fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales").then(response => {
-    return response.json();}).then(data => {
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales").then(response => 
+        response.json()).then(data => {
             const h2 = document.createElement("h2");
             for(let i = 0; i < 2; i++){
                 const {
